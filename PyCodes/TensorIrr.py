8e0f850a187bb40e1153c9dfbb74b7c11270a5c1
ColinsGitCode <xueguodong2016@gmail.com>
2018-06-25 23:34:54 +0900

    default descriptions

--- PyCodes/TensorIrr.py
+++ PyCodes/TensorIrr.py
@@ -2,6 +2,25 @@
 # Filename: TensorIrr.py
 # Usages : Construct the Irregular Tensor
 
+import numpy as np
+import scipy
+
 from SaveData import *
 
+def CreateDefaultSparseDokMatrix(row,col):
+    sm = scipy.sparse.dok_matrix((row,col), dtype=np.float16)
+    for i in range(row):
+        for j in range(col):
+            if i == j:
+                sm[i,j] = i + j
+
+    return sm
+
+def CreateSparseDokMatrix(row,col):
+    sm = scipy.sparse.dok_matrix((row,col), dtype=np.float16)
+    for i in range(row):
+        for j in range(col):
+            if i == (9-j):
+                sm[i,j] = i + j
+    return sm                
 
