fe05d0e17666334e3f2f1ed4356095885924fdaf
ColinsGitCode <xueguodong2016@gmail.com>
2018-07-04 14:11:55 +0900

    default descriptions

--- MatrixTest/matrix.py
+++ MatrixTest/matrix.py
@@ -10,6 +10,7 @@ import numpy as np
 import scipy.sparse as sparse
 import scipy.sparse.linalg as sparse_alg
 from scipy.sparse import csgraph
+import timeit
 
 # ----------------------------------------------------------------------------
 
@@ -51,3 +52,40 @@ def Create2FormatSparseMats(row,col,dense=0.25,type='coo'):
 #                            Zone Title: Sparse Matrix Parts
 # <------------------------- End of the Codes Zone -------------------------->
 # ----------------------------------------------------------------------------
+
+
+def get_dok_sparse_mats(row,col,dense=0.004):
+    m1 = sparse.random(row,col,density=dense,format='dok')
+    m2 = m1.tocoo()
+    return m1,m2
+
+# ------------------------------------------------------------------
+
+def func1(x):
+    pow(x, 2)
+
+def func2(x):
+    return x * x
+
+def dot_time(mat,mat_T):
+    return mat.dot(mat_T)
+
+def transpose_time(mat):
+    return mat.transpose()
+
+def Hadamard_time(mat):
+    return mat.multiply(mat)
+
+
+
+v = 10000
+
+func1_test = 'func1(' + str(v) + ')'
+func2_test = 'func2(' + str(v) + ')'
+print(timeit.timeit(func1_test, 'from __main__ import func1'))
+print(timeit.timeit(func2_test, 'from __main__ import func2'))
+
+print(timeit.repeat(func1_test, 'from __main__ import func1'))
+print(timeit.repeat(func2_test, 'from __main__ import func2'))
+
+
